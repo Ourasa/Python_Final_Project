5982d29910593d7868cc09b22b973f71652ce688
TommyAgarwal <97489592+TommyAgarwal@users.noreply.github.com>
2023-12-05 15:16:15 -0800

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -17,63 +17,17 @@ app = Flask(__name__)
 def home_page():
     return render_template('startup.html')
 
-
-@app.route("/currentWeather")
-def home_to_today_page():
-
+@app.route("/landing")
+def landing_page():
     Retriever.get_current_weather()
     Retriever.get_48hours_temperatures()
     Retriever.get_week_forecast()
     Retriever.get_yesterday_weather()
-
-    #48 HOUR DATA
-    file_name = os.path.join("data", "48hours_temp.txt")
-    timeseries = np.genfromtxt(file_name, skip_header = 2, delimiter="\t")
-    h = [] #hours (x-axis)
-    t = [] #temperature (y-axis)
-    for entry in timeseries:
-        h.append(entry[0])
-        t.append(entry[1])
-    fig = plt.figure()
-    plt.plot(h, t, linewidth=2)
-    plt.grid(linewidth=0.5, alpha=0.8)
-    plt.xlim(-24, 24)
-    plt.xticks([-24, -18, -12, -6, 0, 6, 12, 18, 24])
-    plt.ylabel('Temperature (C)')
-    plt.xlabel('Hour')
-    plt.title('48 Hour Forecast')
-    fig.savefig("static/48hourplot.png")
-    plt.close(fig)
-    #7DAY PLOT 
-    file_name = os.path.join("data", "forecast_weather.txt")
-    timeseries = np.genfromtxt(file_name, skip_header = 1, delimiter="\t")
-    hi_t = [] #high temp (y-axis)
-    lo_t = [] #low temp (y-axis)
-    for entry in timeseries:
-        hi_t.append(entry[0])
-        lo_t.append(entry[1])
-    fig = plt.figure()
-    date = []
-    f = open(file_name, 'r')
-    next(f)
-    for line in f:
-        x = line.split()
-        date.append(x[5])
-    f.close()
-    fig = plt.figure()
-    plt.plot([0,1,2,3,4,5,6], hi_t, 'r', linewidth=2, label = 'High Temperatures')
-    plt.plot([0,1,2,3,4,5,6], lo_t, 'b', linewidth=2, label = 'Low Temperatures')
-    plt.grid(linewidth=0.5, alpha=0.8)
-    plt.xticks([0,1,2,3,4,5,6], date)
-    plt.gcf().autofmt_xdate()
-    plt.ylabel('Temperature (F)')
-    plt.xlabel('Date')
-    plt.title('7 Day Forecast')
-    plt.legend()
-    fig.savefig("static/7dayplot.png")
-    plt.close(fig)
+    return render_template('landing.html')
 
 
+@app.route("/48hourtables")
+def tables_48_hour():
     #CURRENT DATA
     file_name = os.path.join("data", "current_weather.txt")
     f = open(file_name, 'r')
@@ -90,7 +44,6 @@ def home_to_today_page():
     c_time = date_time[1]
     c_forecast_type = forecast[6]
     f.close()
-
     #YESTERDAY DATA
     file_name = os.path.join("data", "yesterday_weather.txt")
     f = open(file_name, 'r')
@@ -104,9 +57,35 @@ def home_to_today_page():
     y_humidity = forecast[4]
     y_date = forecast[5]
     f.close()
+    return render_template('48hourtables.html', c_temp = c_temp, c_apparent_temp = c_apparent_temp, c_precipitation = c_precipitation, c_wind_speed = c_wind_speed, c_humidity = c_humidity, c_date = c_date, c_time = c_time, c_forecast_type = c_forecast_type,
+                           y_hi_temp = y_hi_temp, y_lo_temp = y_lo_temp, y_precipitation = y_precipitation, y_wind_speed = y_wind_speed, y_humidity = y_humidity, y_date = y_date)
 
-    
-    
+
+@app.route("/48hourplot")
+def plot_48_hour():
+    #48 HOUR PLOT
+    file_name = os.path.join("data", "48hours_temp.txt")
+    timeseries = np.genfromtxt(file_name, skip_header = 2, delimiter="\t")
+    h = [] #hours (x-axis)
+    t = [] #temperature (y-axis)
+    for entry in timeseries:
+        h.append(entry[0])
+        t.append(entry[1])
+    fig = plt.figure()
+    plt.plot(h, t, linewidth=2)
+    plt.grid(linewidth=0.5, alpha=0.8)
+    plt.xlim(-24, 24)
+    plt.xticks([-24, -18, -12, -6, 0, 6, 12, 18, 24])
+    plt.ylabel('Temperature (F)')
+    plt.xlabel('Hour')
+    plt.title('48 Hour Forecast')
+    fig.savefig("static/48hourplot.png")
+    plt.close(fig)
+    return render_template('48hourplot.html')
+
+
+@app.route("/7daytable")
+def tables_7_day():
     #7 DAY FORECAST
     file_name = os.path.join("data", "forecast_weather.txt")
     f = open(file_name, 'r')
@@ -181,19 +160,223 @@ def home_to_today_page():
     date7 = forecast[5]
     condition7 = forecast[6]
     f.close()
-
-
-
-
-    return render_template('currentWeather.html', c_temp = c_temp, c_apparent_temp = c_apparent_temp, c_precipitation = c_precipitation, c_wind_speed = c_wind_speed, c_humidity = c_humidity, c_date = c_date, c_time = c_time, c_forecast_type = c_forecast_type,
-                           y_hi_temp = y_hi_temp, y_lo_temp = y_lo_temp, y_precipitation = y_precipitation, y_wind_speed = y_wind_speed, y_humidity = y_humidity, y_date = y_date,
-                            hi_temp1 = hi_temp1, lo_temp1 = lo_temp1, precipitation1 = precipitation1, wind_speed1 = wind_speed1, humidity1 = humidity1, date1 = date1, condition1 = condition1,
+    return render_template('7daytable.html', hi_temp1 = hi_temp1, lo_temp1 = lo_temp1, precipitation1 = precipitation1, wind_speed1 = wind_speed1, humidity1 = humidity1, date1 = date1, condition1 = condition1,
                             hi_temp2 = hi_temp2, lo_temp2 = lo_temp2, precipitation2 = precipitation2, wind_speed2 = wind_speed2, humidity2 = humidity2, date2 = date2, condition2 = condition2,
                             hi_temp3 = hi_temp3, lo_temp3 = lo_temp3, precipitation3 = precipitation3, wind_speed3 = wind_speed3, humidity3 = humidity3, date3 = date3, condition3 = condition3,
                             hi_temp4 = hi_temp4, lo_temp4 = lo_temp4, precipitation4 = precipitation4, wind_speed4 = wind_speed4, humidity4 = humidity4, date4 = date4, condition4 = condition4,
                             hi_temp5 = hi_temp5, lo_temp5 = lo_temp5, precipitation5 = precipitation5, wind_speed5 = wind_speed5, humidity5 = humidity5, date5 = date5, condition5 = condition5,
                             hi_temp6 = hi_temp6, lo_temp6 = lo_temp6, precipitation6 = precipitation6, wind_speed6 = wind_speed6, humidity6 = humidity6, date6 = date6, condition6 = condition6,
                             hi_temp7 = hi_temp7, lo_temp7 = lo_temp7, precipitation7 = precipitation7, wind_speed7 = wind_speed7, humidity7 = humidity7, date7 = date7, condition7 = condition7)
+
+@app.route("/7dayplot")
+def plot_7_day():
+    file_name = os.path.join("data", "forecast_weather.txt")
+    timeseries = np.genfromtxt(file_name, skip_header = 1, delimiter="\t")
+    hi_t = [] #high temp (y-axis)
+    lo_t = [] #low temp (y-axis)
+    for entry in timeseries:
+        hi_t.append(entry[0])
+        lo_t.append(entry[1])
+    fig = plt.figure()
+    date = []
+    f = open(file_name, 'r')
+    next(f)
+    for line in f:
+        x = line.split()
+        date.append(x[5])
+    f.close()
+    fig = plt.figure()
+    plt.plot([0,1,2,3,4,5,6], hi_t, 'r', linewidth=2, label = 'High Temperatures')
+    plt.plot([0,1,2,3,4,5,6], lo_t, 'b', linewidth=2, label = 'Low Temperatures')
+    plt.grid(linewidth=0.5, alpha=0.8)
+    plt.xticks([0,1,2,3,4,5,6], date)
+    plt.gcf().autofmt_xdate()
+    plt.ylabel('Temperature (F)')
+    plt.xlabel('Date')
+    plt.title('7 Day Forecast')
+    plt.legend()
+    fig.savefig("static/7dayplot.png")
+    plt.close(fig)
+    return render_template("7dayplot.html")
+
+
+# @app.route("/currentWeather")
+# def home_to_today_page():
+
+#     Retriever.get_current_weather()
+#     Retriever.get_48hours_temperatures()
+#     Retriever.get_week_forecast()
+#     Retriever.get_yesterday_weather()
+
+#     #48 HOUR PLOT
+#     file_name = os.path.join("data", "48hours_temp.txt")
+#     timeseries = np.genfromtxt(file_name, skip_header = 2, delimiter="\t")
+#     h = [] #hours (x-axis)
+#     t = [] #temperature (y-axis)
+#     for entry in timeseries:
+#         h.append(entry[0])
+#         t.append(entry[1])
+#     fig = plt.figure()
+#     plt.plot(h, t, linewidth=2)
+#     plt.grid(linewidth=0.5, alpha=0.8)
+#     plt.xlim(-24, 24)
+#     plt.xticks([-24, -18, -12, -6, 0, 6, 12, 18, 24])
+#     plt.ylabel('Temperature (C)')
+#     plt.xlabel('Hour')
+#     plt.title('48 Hour Forecast')
+#     fig.savefig("static/48hourplot.png")
+#     plt.close(fig)
+    
+#     #7DAY PLOT 
+#     file_name = os.path.join("data", "forecast_weather.txt")
+#     timeseries = np.genfromtxt(file_name, skip_header = 1, delimiter="\t")
+#     hi_t = [] #high temp (y-axis)
+#     lo_t = [] #low temp (y-axis)
+#     for entry in timeseries:
+#         hi_t.append(entry[0])
+#         lo_t.append(entry[1])
+#     fig = plt.figure()
+#     date = []
+#     f = open(file_name, 'r')
+#     next(f)
+#     for line in f:
+#         x = line.split()
+#         date.append(x[5])
+#     f.close()
+#     fig = plt.figure()
+#     plt.plot([0,1,2,3,4,5,6], hi_t, 'r', linewidth=2, label = 'High Temperatures')
+#     plt.plot([0,1,2,3,4,5,6], lo_t, 'b', linewidth=2, label = 'Low Temperatures')
+#     plt.grid(linewidth=0.5, alpha=0.8)
+#     plt.xticks([0,1,2,3,4,5,6], date)
+#     plt.gcf().autofmt_xdate()
+#     plt.ylabel('Temperature (F)')
+#     plt.xlabel('Date')
+#     plt.title('7 Day Forecast')
+#     plt.legend()
+#     fig.savefig("static/7dayplot.png")
+#     plt.close(fig)
+
+
+#     #CURRENT DATA
+#     file_name = os.path.join("data", "current_weather.txt")
+#     f = open(file_name, 'r')
+#     next(f)
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     c_temp = forecast[0]
+#     c_apparent_temp = forecast[1]
+#     c_precipitation = forecast[2]
+#     c_wind_speed = forecast[3]
+#     c_humidity = forecast[4]
+#     date_time = forecast[5].split()
+#     c_date = date_time[0]
+#     c_time = date_time[1]
+#     c_forecast_type = forecast[6]
+#     f.close()
+
+#     #YESTERDAY DATA
+#     file_name = os.path.join("data", "yesterday_weather.txt")
+#     f = open(file_name, 'r')
+#     next(f)
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     y_hi_temp = forecast[0]
+#     y_lo_temp = forecast[1]
+#     y_precipitation = forecast[2]
+#     y_wind_speed = forecast[3]
+#     y_humidity = forecast[4]
+#     y_date = forecast[5]
+#     f.close()
+
+    
+    
+#     #7 DAY FORECAST
+#     file_name = os.path.join("data", "forecast_weather.txt")
+#     f = open(file_name, 'r')
+#     next(f)
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     hi_temp1 = forecast[0]
+#     lo_temp1 = forecast[1]
+#     precipitation1 = forecast[2]
+#     wind_speed1 = forecast[3]
+#     humidity1 = forecast[4]
+#     date1 = forecast[5]
+#     condition1 = forecast[6]
+
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     hi_temp2 = forecast[0]
+#     lo_temp2 = forecast[1]
+#     precipitation2 = forecast[2]
+#     wind_speed2 = forecast[3]
+#     humidity2 = forecast[4]
+#     date2 = forecast[5]
+#     condition2 = forecast[6]
+
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     hi_temp3 = forecast[0]
+#     lo_temp3 = forecast[1]
+#     precipitation3 = forecast[2]
+#     wind_speed3 = forecast[3]
+#     humidity3 = forecast[4]
+#     date3 = forecast[5]
+#     condition3 = forecast[6]
+
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     hi_temp4 = forecast[0]
+#     lo_temp4 = forecast[1]
+#     precipitation4 = forecast[2]
+#     wind_speed4 = forecast[3]
+#     humidity4 = forecast[4]
+#     date4 = forecast[5]
+#     condition4 = forecast[6]
+
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     hi_temp5 = forecast[0]
+#     lo_temp5 = forecast[1]
+#     precipitation5 = forecast[2]
+#     wind_speed5 = forecast[3]
+#     humidity5 = forecast[4]
+#     date5 = forecast[5]
+#     condition5 = forecast[6]
+
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     hi_temp6 = forecast[0]
+#     lo_temp6 = forecast[1]
+#     precipitation6 = forecast[2]
+#     wind_speed6 = forecast[3]
+#     humidity6 = forecast[4]
+#     date6 = forecast[5]
+#     condition6 = forecast[6]
+
+#     line = f.readline()
+#     forecast = line.split('\t')
+#     hi_temp7 = forecast[0]
+#     lo_temp7 = forecast[1]
+#     precipitation7 = forecast[2]
+#     wind_speed7 = forecast[3]
+#     humidity7 = forecast[4]
+#     date7 = forecast[5]
+#     condition7 = forecast[6]
+#     f.close()
+
+
+
+
+#     return render_template('currentWeather.html', c_temp = c_temp, c_apparent_temp = c_apparent_temp, c_precipitation = c_precipitation, c_wind_speed = c_wind_speed, c_humidity = c_humidity, c_date = c_date, c_time = c_time, c_forecast_type = c_forecast_type,
+#                            y_hi_temp = y_hi_temp, y_lo_temp = y_lo_temp, y_precipitation = y_precipitation, y_wind_speed = y_wind_speed, y_humidity = y_humidity, y_date = y_date,
+#                             hi_temp1 = hi_temp1, lo_temp1 = lo_temp1, precipitation1 = precipitation1, wind_speed1 = wind_speed1, humidity1 = humidity1, date1 = date1, condition1 = condition1,
+#                             hi_temp2 = hi_temp2, lo_temp2 = lo_temp2, precipitation2 = precipitation2, wind_speed2 = wind_speed2, humidity2 = humidity2, date2 = date2, condition2 = condition2,
+#                             hi_temp3 = hi_temp3, lo_temp3 = lo_temp3, precipitation3 = precipitation3, wind_speed3 = wind_speed3, humidity3 = humidity3, date3 = date3, condition3 = condition3,
+#                             hi_temp4 = hi_temp4, lo_temp4 = lo_temp4, precipitation4 = precipitation4, wind_speed4 = wind_speed4, humidity4 = humidity4, date4 = date4, condition4 = condition4,
+#                             hi_temp5 = hi_temp5, lo_temp5 = lo_temp5, precipitation5 = precipitation5, wind_speed5 = wind_speed5, humidity5 = humidity5, date5 = date5, condition5 = condition5,
+#                             hi_temp6 = hi_temp6, lo_temp6 = lo_temp6, precipitation6 = precipitation6, wind_speed6 = wind_speed6, humidity6 = humidity6, date6 = date6, condition6 = condition6,
+#                             hi_temp7 = hi_temp7, lo_temp7 = lo_temp7, precipitation7 = precipitation7, wind_speed7 = wind_speed7, humidity7 = humidity7, date7 = date7, condition7 = condition7)
 
 
 # create a main method
